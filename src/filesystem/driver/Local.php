b770fe2277757d593d89a7a47956064a1354808a
axguowen <axguowen@qq.com>
2023-02-14 22:13:35 +0800

    Initial commit

---
+++ src/filesystem/driver/Local.php
@@ -0,0 +1,58 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2021 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: yunwuxin <448901948@qq.com>
+// +----------------------------------------------------------------------
+
+namespace think\filesystem\driver;
+
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Adapter\Local as LocalAdapter;
+use think\filesystem\Driver;
+
+class Local extends Driver
+{
+    /**
+     * 配置参数
+     * @var array
+     */
+    protected $config = [
+        'root' => '',
+    ];
+
+    protected function createAdapter()
+    {
+        $permissions = isset($this->config['permissions']) ? $this->config['permissions'] : [];
+
+        $links = (isset($this->config['links']) ? $this->config['links'] : null) === 'skip'
+        ? LocalAdapter::SKIP_LINKS
+        : LocalAdapter::DISALLOW_LINKS;
+
+        return new LocalAdapter(
+            $this->config['root'],
+            LOCK_EX,
+            $links,
+            $permissions
+        );
+    }
+
+    /**
+     * 获取文件访问地址
+     * @param string $path 文件路径
+     * @return string
+     */
+    public function url($path)
+    {
+        $path = str_replace('\\', '/', $path);
+
+        if (isset($this->config['url'])) {
+            return $this->concatPathToUrl($this->config['url'], $path);
+        }
+        return parent::url($path);
+    }
+}
